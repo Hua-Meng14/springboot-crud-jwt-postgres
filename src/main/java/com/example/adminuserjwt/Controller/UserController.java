588f38b00a83660231e92fc4f81cc70c4cc1b167
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-06-01 11:07:39 +0700

    user controller

--- src/main/java/com/example/adminuserjwt/Controller/UserController.java
+++ src/main/java/com/example/adminuserjwt/Controller/UserController.java
@@ -1,4 +1,19 @@
 package com.example.adminuserjwt.Controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> get() {return ResponseEntity.ok("GET:: user controller");}
+    @PostMapping
+    public ResponseEntity<String> post() {return ResponseEntity.ok("POST:: user controller");}
+    @PutMapping
+    public ResponseEntity<String> update() {return ResponseEntity.ok("UPDATE:: user controller");}
+    @DeleteMapping
+    public ResponseEntity<String> delete() {return ResponseEntity.ok("DELETE:: user controller");}
 }
