dd24fe33f99c20e635726242b069cd9a8592eed3
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-06-01 14:52:38 +0700

    comment used code

--- src/main/java/com/example/adminuserjwt/user/User.java
+++ src/main/java/com/example/adminuserjwt/user/User.java
@@ -32,27 +32,27 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Token> tokens;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities(){
-        return role.getAuthorities();
-    }
-
-    @Override
-    public String getPassword() {return password;}
-
-    @Override
-    public String getEmail() { return email;}
-
-    @Override
-    public boolean isAccountNonExpired() {return true;}
-
-    @Override
-    public boolean isAccountNonLocked() {return true;}
-
-    @Override
-    public boolean isCredentialsNonExpired() {return true;}
-
-    @Override
-    public boolean isEnabled() {return true;}
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return role.getAuthorities();
+//    }
+//
+//    @Override
+//    public String getPassword() {return password;}
+//
+//    @Override
+//    public String getEmail() { return email;}
+//
+//    @Override
+//    public boolean isAccountNonExpired() {return true;}
+//
+//    @Override
+//    public boolean isAccountNonLocked() {return true;}
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {return true;}
+//
+//    @Override
+//    public boolean isEnabled() {return true;}
 
 }
