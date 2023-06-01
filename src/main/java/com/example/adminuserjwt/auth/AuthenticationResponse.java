d75d6c8ff284bad9cb6690737011d5d555bea83d
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-06-01 11:22:02 +0700

    authentication response model

--- src/main/java/com/example/adminuserjwt/auth/AuthenticationResponse.java
+++ src/main/java/com/example/adminuserjwt/auth/AuthenticationResponse.java
@@ -1,2 +1,20 @@
-package com.example.adminuserjwt.auth;public class AuthenticationResponse {
+package com.example.adminuserjwt.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh-token")
+    private String refreshToken;
 }
